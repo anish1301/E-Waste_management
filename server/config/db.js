7354bd49999470c7daaafc2955032cd63db09e04
anish1301 <145433865+anish1301@users.noreply.github.com>
2024-10-14 18:25:27 +0530

    Adding all the files

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,5 @@
 const mongoose = require ('mongoose')
-
 const connect = () => {
-    return mongoose.connect('mongodb+srv://girishv694:8762331996@cluster0.l8qod.mongodb.net/Ewaste?retryWrites=true&w=majority')
+    return mongoose.connect('mongodb+srv://anishkumar130119:tcmec2vfkr2YCz6B@cluster0.pojxz.mongodb.net/')
 }
 module.exports = connect;
